a8348d740b35a95102c24b206cb59d06ea206e67
shangeth <shangethrajaa@gmail.com>
2020-08-08 00:33:43 +0530

    training tensorboard time commit

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import time
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
-import datetime
+from datetime import datetime
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -34,7 +34,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
 
 #---------------------------------
 
-writer = SummaryWriter(f'logging/runs/{datetime.now().strftime("%m/%d/%Y-%H:%M:%S")}')
+writer = SummaryWriter(f'logging/runs/{datetime.now().strftime("%m-%d-%Y_%H:%M:%S")}')
 # no of epochs
 epochs = 20
 
