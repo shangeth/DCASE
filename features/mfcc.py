f333df421ff8dcee4dc158dc569efb422e34ba89
shangeth <shangethrajaa@gmail.com>
2020-08-09 06:50:10 +0530

    mfcc features commit

--- features/mfcc.py
+++ features/mfcc.py
@@ -6,14 +6,14 @@ import torchaudio
 import torch
 
 def get_mfcc(DATA_PATH, SAVE_DIR, resample=16000):
-    audio_list = os.listdir(DATA_PATH, SAVE_DIR)
+    audio_list = os.listdir(DATA_PATH)
     for name in tqdm(audio_list):
         arr_name = name.split('.')[0]
-        waveform, sample_rate = librosa.load(DATA_PATH+'/'+name, sr=16000)
+        waveform, sample_rate = librosa.load(DATA_PATH+'/'+name, sr=resample)
         arr = torchaudio.transforms.MFCC(sample_rate=sample_rate)(torch.tensor(waveform)).numpy()
         np.save(SAVE_DIR+'/'+arr_name+'.npy', arr)
 
 if __name__ == "__main__":
-    DATA_PATH = ''
-    SAVE_DIR = ''
+    DATA_PATH = 'data/audio'
+    SAVE_DIR = 'data/features'
     get_mfcc(DATA_PATH, SAVE_DIR)
